e2993483d0ab48ffd14329107ab2817b28bcba46
Elena Yan <elena.yan@emse.fr>
2026-09-21 19:24:48 +0200

    feat: rmcon agent

---
+++ src/agt/regulation/rmcon.asl
@@ -0,0 +1,35 @@
+// Regulation Management
+
+
++active(obligation(Me, M, O,D)) : .my_name(Me)
+    <-  .print("I am obliged to achieve: ", O);
+        .
+
++active(obligation(Ag, M, O,D))
+    <-  .print(Ag, " is obliged to achieve: ", O);
+        .send(Ag, achieve, O);
+        .print("sent obligation to ", Ag);
+        .
+
++Instance[source(npli)]
+    <-  .print("NPL FACT: ", Instance) .
+
++Constitutive[source(np)]
+    <-  .print("SAI FACT: ", Constitutive) .
+
++active(O)
+    <-  .print("active obligation: ", O);
+        .
+
++fulfilled(O)
+    <-  .print("fulfilled obligation: ", O).
+
++unfulfilled(O)
+    <-  .print("unfulfilled obligation: ", O).
+
+
+{ include("$jacamo/templates/common-cartago.asl") }
+{ include("$jacamo/templates/common-moise.asl") }
+
+// uncomment the include below to have an agent compliant with its organisation
+//{ include("$moise/asl/org-obedient.asl") }
---
+++ src/env/RmArtifact.java
@@ -0,0 +1,59 @@
+import cartago.Artifact;
+import cartago.OPERATION;
+import jason.asSyntax.Atom;
+import jason.asSyntax.Literal;
+import jason.asSyntax.Term;
+import jason.asSyntax.parser.ParseException;
+
+import static jason.asSyntax.ASSyntax.parseLiteral;
+
+public class RmArtifact extends Artifact {
+
+    private static final Term RM_ATOM = new Atom("rm");;
+
+    void init() {
+        log("Artifact initialized");
+    }
+
+    @OPERATION
+    public void addSignal(String event) {
+        try {
+            Literal l = parseLiteral(event);
+            signal(RM_ATOM.toString(), l);
+            log("SIGNAL::: " + l + " " + l.getSources());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OPERATION
+    public void addSkFact(String fact) {
+        try {
+            Literal l = parseLiteral(fact);
+//            l.addSource(RM_ATOM);
+            if (l.hasTerm()) {
+
+                log("ADD FACT::: " + l.getFunctor() + " " + l.getTerms());
+                defineObsProperty(l.getFunctor(), l.getTerms());
+            } else  {
+                defineObsProperty(l.getFunctor());
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OPERATION
+    public void removeSkFact(String fact) {
+        try {
+            Literal l = parseLiteral(fact);
+            if (l.hasTerm()) {
+                removeObsPropertyByTemplate(l.getFunctor(), l.getTerms());
+            } else {
+                removeObsProperty(l.getFunctor());
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
